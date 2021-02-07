8c6abade050179bf3c5e544ee8fa16231ad89e94
DeepJha <deep.jhav@gmail.com>
2021-02-07 18:22:23 +0530

    v1.0.0 basic structure created

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "../main.scss";
+
+//COMPONENTS IMPORT
+import SideNavbar from "./SideNavbar";
+/////////////////////////////////////////////
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <SideNavbar />
+      </>
+    );
+  }
+}
+///////////////////////////////////////////////
+export default App;
---
+++ src/components/SideNavbar.js
@@ -0,0 +1,25 @@
+import React from "react";
+/////////////////////////////////////////
+class SideNavbar extends React.Component {
+  render() {
+    return (
+      <div className="sidenav">
+        <div className="sidenav__top">
+          <div id="nav-component"></div>
+          <div id="nav-component"></div>
+          <div id="nav-component"></div>
+          <div id="nav-component"></div>
+          <div id="nav-component"></div>
+          <div id="nav-component"></div>
+          <div id="nav-component"></div>
+        </div>
+        <div className="sidenav__bottom">
+          <div id="nav-component"></div>
+          <div id="nav-component"></div>
+        </div>
+      </div>
+    );
+  }
+}
+//////////////////////////////////////////
+export default SideNavbar;
